0689356f953ba0d812d6faee6bea953c6b3d341b
Vivek Kumar <vivek.kumar5@lectrixev.com>
2024-11-15 23:35:45 +0530

    db changes

--- server/src/Databases/index.ts
+++ server/src/Databases/index.ts
@@ -1,6 +1,8 @@
 import { DB_DATABASE, DB_PASSWORD, DB_USERNAME } from '@/Config';
 import { ConnectOptions } from 'mongoose';
 
+// mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@websitecluster.pg06qgw.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority
+
 const options: ConnectOptions = {
   autoCreate: true,
   autoIndex: true,
@@ -9,5 +11,5 @@ const options: ConnectOptions = {
 };
 export const dbConnection = {
   options,
-  url: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@websitecluster.pg06qgw.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority`,
+  url: `mongodb+srv://root:Dgxg01FBoKTP0nUT@ev.hfsqrfg.mongodb.net/MoovingDBStage?retryWrites=true&w=majority`,
 };
