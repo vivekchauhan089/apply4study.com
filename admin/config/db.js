4ff76ddca22f57fa94482bbd5d5e6843362d03d8
Navdhara <er.vivek.rgec.cs@gmail.com>
2025-10-22 14:12:47 +0530

    misc changes

--- admin/config/db.js
+++ admin/config/db.js
@@ -5,8 +5,7 @@ global.url = "mongodb+srv://lmsuser:Phph73g9d5sjWiTf@ev.hfsqrfg.mongodb.net/lms?
 // Connecting to the database
 mongoose.connect(url, {
     useNewUrlParser: true,
-    ssl: true,
-    tlsAllowInvalidCertificates: false // true only for testing self-signed
+    ssl: true
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
